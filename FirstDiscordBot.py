ed7eb529d7383adb39d26851129f096353442776
Redwanul Abedin <chromeplated@protonmail.com>
2024-06-09 15:48:58 -0400

    Initial commit

---
+++ FirstDiscordBot.py
@@ -0,0 +1,25 @@
+# Imports
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# bringing in the token safely
+token = open("token.txt", "r")
+
+client.run(f.read())
